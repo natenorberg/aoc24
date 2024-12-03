1114c1d2d894f066879e2d0457dbc962d0611170
Nate Norberg <nate.norberg@quiq.com>
2024-12-03 08:53:30 -0700

    Day 3

--- day03/day03.test.ts
+++ day03/day03.test.ts
@@ -8,7 +8,7 @@ test('Part1 test answer', async () => {
 
 test('findInstructions', () => {
   const input = 'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))';
-  expect(findInstructions(input)).toEqual(['mul(2,4)', 'mul(5,5)', 'mul(11,8)', 'mul(8,5)']);
+  expect(findInstructions(input, true)).toEqual(['mul(2,4)', 'mul(5,5)', 'mul(11,8)', 'mul(8,5)']);
 });
 
 test('Part1 real answer', async () => {
@@ -16,12 +16,12 @@ test('Part1 real answer', async () => {
   expect(answer).toBe(173517243);
 });
 
-// test('Part2 test answer', async () => {
-//   const answer = await Day03.Part2Answer('test-input.txt');
-//   expect(answer).toBe(4);
-// });
+test('Part2 test answer', async () => {
+  const answer = await Day03.Part2Answer('test-input2.txt');
+  expect(answer).toBe(48);
+});
 
-// test('Part2 real answer', async () => {
-//   const answer = await Day03.Part2Answer('input.txt');
-//   expect(answer).toBe(426);
-// });
+test('Part2 real answer', async () => {
+  const answer = await Day03.Part2Answer('input.txt');
+  expect(answer).toBe(100450138);
+});
--- day03/day03.ts
+++ day03/day03.ts
@@ -1,40 +1,70 @@
-import {readLines, readTextFile} from '../utils';
+import {readTextFile} from '../utils';
 
 export const Day03 = {
   async Part1Answer(filename: string) {
     const input = await readTextFile(filename);
-    const instructions = parseInstructions(input);
+    const instructions = parseInstructions(input, false);
     let total = 0;
-    instructions.forEach(({x, y}) => {
-      total += x * y;
+    instructions.forEach((instruction) => {
+      if (instruction.type === 'multiply') {
+        const {x, y} = instruction;
+        total += x * y;
+      }
     });
     return total;
   },
   async Part2Answer(filename: string) {
-    const lines = await readLines(filename);
-    return 0;
+    const input = await readTextFile(filename);
+    const instructions = parseInstructions(input, true);
+    let total = 0;
+    let enabled = true;
+    instructions.forEach((instruction) => {
+      switch (instruction.type) {
+        case 'do':
+          enabled = true;
+          break;
+        case 'dont':
+          enabled = false;
+          break;
+        case 'multiply':
+          if (enabled) {
+            const {x, y} = instruction;
+            total += x * y;
+          }
+      }
+    });
+    return total;
   },
 };
 
-type Instruction = {x: number; y: number};
+type MultiplyInstruction = {type: 'multiply'; x: number; y: number};
+type Instruction = MultiplyInstruction | {type: 'do'} | {type: 'dont'};
 
-function parseInstructions(input: string): Instruction[] {
-  const instructionStrings = findInstructions(input);
+function parseInstructions(input: string, includeConditionals: boolean): Instruction[] {
+  const instructionStrings = findInstructions(input, includeConditionals);
   return instructionStrings.map(parseInstruction);
 }
 
 function parseInstruction(input: string): Instruction {
+  if (input.startsWith("don't")) {
+    return {type: 'dont'};
+  }
+  if (input.startsWith('do')) {
+    return {type: 'do'};
+  }
   const numbers = input
     .replace('mul(', '')
     .replace(')', '')
     .split(',')
     .map((n) => Number.parseInt(n));
-  return {x: numbers[0], y: numbers[1]};
+  return {type: 'multiply', x: numbers[0], y: numbers[1]};
 }
 
-export function findInstructions(input: string) {
-  const regex = /mul\(\d{1,3},\d{1,3}\)/g;
+export function findInstructions(input: string, includeConditionals: boolean) {
+  const regex = includeConditionals
+    ? /mul\(\d{1,3},\d{1,3}\)|do\(\)|don\'t\(\)/g
+    : /mul\(\d{1,3},\d{1,3}\)/g;
   return Array.from(input.matchAll(regex)).map((v) => v[0]);
 }
 
-console.log(await Day03.Part1Answer('input.txt'));
+console.log(await Day03.Part2Answer('input.txt'));
