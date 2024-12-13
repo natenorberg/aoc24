0580e9b767d7d991357bc537c290325ef5a844ac
Nate Norberg <nate.norberg@quiq.com>
2024-12-13 09:25:11 -0700

    Day 13 part 1

---
+++ day13/day13.test.ts
@@ -0,0 +1,44 @@
+import {test, expect} from 'bun:test';
+import {Day13, parseMachines, rowReduce} from './day13';
+import {readTextFile} from '../utils';
+
+test('Part1 test answer', async () => {
+  const answer = await Day13.Part1Answer('test-input.txt');
+  expect(answer).toBe(480);
+});
+
+test('Part1 real answer', async () => {
+  const answer = await Day13.Part1Answer('input.txt');
+  expect(answer).toBe(36870);
+});
+
+test.skip('Part2 test answer', async () => {
+  const answer = await Day13.Part2Answer('test-input.txt');
+  expect(answer).toBe(1);
+});
+
+test.skip('Part2 real answer', async () => {
+  const answer = await Day13.Part2Answer('input.txt');
+  expect(answer).toBe(1);
+});
+
+test('parseMachines', async () => {
+  const input = await readTextFile('test-input.txt');
+  const [machine] = parseMachines(input);
+  expect(machine).toEqual({
+    A: {x: 94, y: 34},
+    B: {x: 22, y: 67},
+    prize: {x: 8400, y: 5400},
+  });
+});
+
+test('rowReduce', () => {
+  let input = [
+    [17, 84, 7870],
+    [86, 37, 6450],
+  ];
+  expect(rowReduce(input)).toEqual([
+    [1, 0, 38],
+    [-0, 1, 86],
+  ]);
+});
---
+++ day13/day13.ts
@@ -0,0 +1,112 @@
+import {readTextFile, sumByFunc} from '../utils';
+
+export const Day13 = {
+  async Part1Answer(filename: string) {
+    const input = await readTextFile(filename);
+    const machines = parseMachines(input);
+    return sumByFunc(machines, findCost);
+  },
+  async Part2Answer(filename: string) {
+    const input = await readTextFile(filename);
+    return 0;
+  },
+};
+
+type Coordinates = {x: number; y: number};
+
+type Machine = {
+  A: Coordinates;
+  B: Coordinates;
+  prize: Coordinates;
+};
+
+type Presses = {A: number; B: number};
+
+/**
+ * Takes a machine and uses RREF to find the values of A and B presses that actually get to the target
+ * This is a system of linear equations problem, but we also need to make sure that we only accept integers as our solution
+ */
+function getPresses(machine: Machine): Presses | undefined {
+  const matrix = [
+    [machine.A.x, machine.B.x, machine.prize.x],
+    [machine.A.y, machine.B.y, machine.prize.y],
+  ];
+
+  rowReduce(matrix);
+  const A = matrix[0][2];
+  const B = matrix[1][2];
+  const aRounded = roundReallyCloseNumber(A);
+  const bRounded = roundReallyCloseNumber(B);
+
+  if (aRounded !== undefined && bRounded !== undefined) {
+    return {A: aRounded, B: bRounded};
+  }
+}
+
+/**
+ * When doing math, you can end up with a number that's barely not an int
+ * If it's close enough, this will return the real int
+ */
+function roundReallyCloseNumber(value: number) {
+  const tolerance = 1e-10;
+  const valueAsInt = Math.round(value);
+  if (Math.abs(value - valueAsInt) < tolerance) {
+    return valueAsInt;
+  }
+}
+
+/**
+ * Simplified RREF that only needs to work for these 2x3 matrices
+ */
+export function rowReduce(matrix: number[][]): number[][] {
+  // Divide the first row by the first number in the first row
+  const pivot = matrix[0][0];
+  matrix[0].forEach((n, i) => (matrix[0][i] = n / pivot));
+
+  // Subtract the first row times [1][0] from the second row
+  let scale = matrix[1][0];
+  matrix[1].forEach((n, i) => (matrix[1][i] = n - matrix[0][i] * scale));
+
+  // Multiply second row by (1 / [1][1])
+  scale = 1 / matrix[1][1];
+  matrix[1].forEach((n, i) => (matrix[1][i] = n * scale));
+
+  // Subtract the second row times [0][1] from the first row
+  scale = matrix[0][1];
+  matrix[0].forEach((n, i) => (matrix[0][i] = n - matrix[1][i] * scale));
+
+  return matrix;
+}
+
+// Scoring =====================================================================
+function findCost(machine: Machine): number {
+  const presses = getPresses(machine);
+  if (!presses) return 0;
+
+  return presses.A * 3 + presses.B;
+}
+
+// Parsing =====================================================================
+export function parseMachines(input: string): Machine[] {
+  return input
+    .split('\n\n')
+    .map((s) => s.split('\n'))
+    .map(parseMachine);
+}
+
+function parseMachine(input: string[]): Machine {
+  return {
+    A: parseLine(input[0], '+'),
+    B: parseLine(input[1], '+'),
+    prize: parseLine(input[2], '='),
+  };
+}
+
+function parseLine(line: string, splitChar: string): Coordinates {
+  const parts = line.split(splitChar);
+  const x = parts[1].replace(', Y', '');
+  const y = parts[2];
+  return {x: Number.parseInt(x), y: Number.parseInt(y)};
+}
+
+// console.log(await Day13.Part1Answer('input.txt'));
