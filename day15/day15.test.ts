ff8eac2f708b540dc7913918a6d33002a03f1dfe
Nate Norberg <nate.norberg@quiq.com>
2024-12-15 17:03:30 -0700

    Day 15 part 1

---
+++ day15/day15.test.ts
@@ -0,0 +1,115 @@
+import {test, expect} from 'bun:test';
+import {Day15, determineMove, Direction, Item} from './day15';
+
+test('Part1 small test answer', async () => {
+  const answer = await Day15.Part1Answer('small-test-input.txt');
+  expect(answer).toBe(2028);
+});
+
+test('Part1 large test answer', async () => {
+  const answer = await Day15.Part1Answer('large-test-input.txt');
+  expect(answer).toBe(10092);
+});
+
+test('Part1 real answer', async () => {
+  const answer = await Day15.Part1Answer('input.txt');
+  expect(answer).toBe(1509863);
+});
+
+test.skip('Part2 test answer', async () => {
+  const answer = await Day15.Part2Answer('test-input.txt');
+  expect(answer).toBe(1);
+});
+
+test.skip('Part2 real answer', async () => {
+  const answer = await Day15.Part2Answer('input.txt');
+  expect(answer).toBe(1);
+});
+
+function getTestMap(input: string): Item[][] {
+  return input
+    .trim()
+    .split('\n')
+    .map((line) => line.split('') as Item[]);
+}
+const testMap1 = getTestMap(`
+########
+#..O.O.#
+##@.O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+`);
+
+test('determineMove blocked', () => {
+  expect(determineMove({row: 2, col: 2}, Direction.Left, testMap1)).toEqual({
+    type: 'blocked',
+  });
+});
+
+test('determineMove simple move', () => {
+  expect(determineMove({row: 2, col: 2}, Direction.Up, testMap1)).toEqual({
+    type: 'move',
+    to: {row: 1, col: 2},
+  });
+});
+
+const push1TestMap = getTestMap(`
+########
+#.@O.O.#
+##..O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+`);
+
+test('determineMove push 1 box', () => {
+  expect(determineMove({row: 1, col: 2}, Direction.Right, push1TestMap)).toEqual({
+    type: 'push',
+    boxes: [{row: 1, col: 4}],
+    to: {row: 1, col: 3},
+  });
+});
+
+const push2TestMap = getTestMap(`
+########
+#..@OO.#
+##..O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+`);
+
+test('determineMove push multiple boxes', () => {
+  expect(determineMove({row: 1, col: 3}, Direction.Right, push2TestMap)).toEqual({
+    type: 'push',
+    boxes: [
+      {row: 1, col: 6},
+      {row: 1, col: 5},
+    ],
+    to: {row: 1, col: 4},
+  });
+});
+
+const blockedPushTestMap = getTestMap(`
+########
+#...@OO#
+##..O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+`);
+
+test('determineMove blocked push', () => {
+  expect(determineMove({row: 1, col: 4}, Direction.Right, blockedPushTestMap)).toEqual({
+    type: 'blocked',
+  });
+});
---
+++ day15/day15.ts
@@ -0,0 +1,147 @@
+import {readTextFile} from '../utils';
+
+export const Day15 = {
+  async Part1Answer(filename: string) {
+    const input = await readTextFile(filename);
+    const {map, directions} = parseInput(input);
+    const finalMap = runDirections(map, directions);
+    return getGpsCoordinates(finalMap);
+  },
+  async Part2Answer(filename: string) {
+    const input = await readTextFile(filename);
+    return 0;
+  },
+};
+
+export enum Item {
+  Robot = '@',
+  Box = 'O',
+  Wall = '#',
+  Empty = '.',
+}
+
+export enum Direction {
+  Up = '^',
+  Down = 'v',
+  Left = '<',
+  Right = '>',
+}
+
+type Point = {row: number; col: number};
+
+function runDirections(initialMap: Item[][], directions: Direction[]): Item[][] {
+  let map = initialMap;
+  directions.forEach((direction) => {
+    map = nextMap(map, direction);
+  });
+  return map;
+}
+
+function nextMap(map: Item[][], direction: Direction): Item[][] {
+  const currentPoint = getCurrentPoint(map);
+  const nextMove = determineMove(currentPoint, direction, map);
+  if (nextMove.type === 'blocked') {
+    return map;
+  }
+  if (nextMove.type === 'push') {
+    nextMove.boxes.forEach((b) => {
+      map[b.row][b.col] = Item.Box;
+    });
+  }
+
+  map[nextMove.to.row][nextMove.to.col] = Item.Robot;
+  map[currentPoint.row][currentPoint.col] = Item.Empty;
+
+  return map;
+}
+
+function getCurrentPoint(map: Item[][]): Point {
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[0].length; j++) {
+      if (map[i][j] === Item.Robot) {
+        return {row: i, col: j};
+      }
+    }
+  }
+  throw new Error('lost the bot');
+}
+
+type BlockedMove = {type: 'blocked'};
+type SimpleMove = {type: 'move'; to: Point};
+type PushMove = {type: 'push'; boxes: Point[]; to: Point};
+type Move = BlockedMove | SimpleMove | PushMove;
+
+export function determineMove(from: Point, direction: Direction, map: Item[][]): Move {
+  const nextPoint = getNextPoint(from, direction);
+  const nextItem = map[nextPoint.row][nextPoint.col];
+
+  switch (nextItem) {
+    case Item.Wall:
+      return {type: 'blocked'};
+    case Item.Empty:
+      return {type: 'move', to: nextPoint};
+    case Item.Box:
+      const pushResult = determineMove(nextPoint, direction, map);
+      switch (pushResult.type) {
+        case 'blocked':
+          return {type: 'blocked'};
+        case 'move':
+          return {type: 'push', boxes: [pushResult.to], to: nextPoint};
+        case 'push':
+          return {type: 'push', boxes: [...pushResult.boxes, pushResult.to], to: nextPoint};
+      }
+    default:
+      throw new Error('Invalid type');
+  }
+}
+
+function getNextPoint(point: Point, direction: Direction): Point {
+  switch (direction) {
+    case Direction.Up:
+      return {...point, row: point.row - 1};
+    case Direction.Down:
+      return {...point, row: point.row + 1};
+    case Direction.Left:
+      return {...point, col: point.col - 1};
+    case Direction.Right:
+      return {...point, col: point.col + 1};
+  }
+}
+
+// Scoring =====================================================================
+function getGpsCoordinates(map: Item[][]): number {
+  let total = 0;
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[0].length; j++) {
+      if (map[i][j] === Item.Box) {
+        total += 100 * i + j;
+      }
+    }
+  }
+
+  return total;
+}
+
+// Parsing =====================================================================
+function parseInput(input: string) {
+  const parts = input.split('\n\n');
+  return {
+    map: parseMap(parts[0].split('\n')),
+    directions: parseDirections(parts[1]),
+  };
+}
+
+function parseMap(input: string[]): Item[][] {
+  return input.map((row) => row.split('').map((item) => item as Item));
+}
+
+function parseDirections(input: string): Direction[] {
+  return input.split('').filter((d) => Object.values(Direction).includes(d as any)) as Direction[];
+}
+
+// Debug =======================================================================
+function printMap(map: Item[][]) {
+  console.log(map.map((line) => line.join('')).join('\n'));
+}
+
+console.log(await Day15.Part1Answer('input.txt'));
